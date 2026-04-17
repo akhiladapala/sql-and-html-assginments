2d9359c6214545170473ac2bac7cbee0005a793c
akhiladapala <108956192+akhiladapala@users.noreply.github.com>
2026-04-17 17:59:59 +0530

    Add files via upload

---
+++ class 1/first.js
@@ -0,0 +1,11 @@
+function show(){
+    document.body.style.backgroundColor="Orange";
+    alert('Form submitted!!')
+}
+
+let e1 = document.getElementById("div1")
+e1.style.color="red";
+
+let items = document.getElementsByClassName("class1");
+items[0].style.color="white";
+items[2].style.color="gold";
